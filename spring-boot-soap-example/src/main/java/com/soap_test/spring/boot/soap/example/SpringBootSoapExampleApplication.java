27fe06941f9b6cb863af069c5195a67b532f226e
Dario <dario_gow3@hotmail.com>
2019-09-23 17:19:55 -0500

    SOAP test firts commit

---
+++ spring-boot-soap-example/src/main/java/com/soap_test/spring/boot/soap/example/SpringBootSoapExampleApplication.java
@@ -0,0 +1,13 @@
+package com.soap_test.spring.boot.soap.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapExampleApplication.class, args);
+	}
+
+}
