78df33df03a9cc7c8eb83873d6f8bc0514dc9f78
Dario <dario_gow3@hotmail.com>
2019-10-02 00:15:36 -0500

    correcciones

--- royalholidayclient/src/main/java/com/royalholiday/ws/royalholidayclient/RoyalholidayclientApplication.java
+++ royalholidayclient/src/main/java/com/royalholiday/ws/royalholidayclient/RoyalholidayclientApplication.java
@@ -3,6 +3,7 @@ package com.royalholiday.ws.royalholidayclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import generated.SearchOwnerRQ;
 import wsclient.OwnerInfoClient;
 
 @SpringBootApplication
@@ -11,8 +12,25 @@ public class RoyalholidayclientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RoyalholidayclientApplication.class, args);
 		
+		String cadenaBusqueda = "12345";
+		String tipoContacto = "12345";
+		String ejecutivo = "";
+		String token = "0";
+		int log = 1;
+		int almacenar = 1;
+		boolean live = false;
+		
+		SearchOwnerRQ setInfo = new SearchOwnerRQ();
+		setInfo.setCadenaBusqueda(cadenaBusqueda);
+		setInfo.setTipoContacto(tipoContacto);
+		setInfo.setLogPeticionID(log);
+		setInfo.setAlmacenar(almacenar);
+		setInfo.setEjecutivo(ejecutivo);
+		setInfo.setToken(token);
+		setInfo.setLive(live);
+		
 		OwnerInfoClient test = new OwnerInfoClient();
-		test.setSearchParams();
+		test.setSearchParams(setInfo);
 	}
 
 }
--- royalholidayclient/src/main/java/wsclient/OwnerInfoClient.java
+++ royalholidayclient/src/main/java/wsclient/OwnerInfoClient.java
@@ -1,38 +1,16 @@
 package wsclient;
 
-import generated.OwnerInformation;
-import generated.OwnerInformationSoap;
-import generated.SearchOwner;
+import generated.ArrayOfInfoCuenta;
 import generated.SearchOwnerRQ;
-import generated.SearchOwnerResponse;
+import generated.SearchOwnerRS;
 
 public class OwnerInfoClient{
 	
-	public void setSearchParams() {
+	public ArrayOfInfoCuenta setSearchParams(SearchOwnerRQ info) {
 		
-		String cadenaBusqueda = "12345";
-		String tipoContacto = "12345";
-		String ejecutivo = "";
-		String token = "0";
-		int log = 1;
-		int almacenar = 1;
-		boolean live = false;
+		SearchOwnerRS response = new SearchOwnerRS();
+		response.setParametroBuscar(info);
+		return response.getInfoCuenta();
 		
-		SearchOwnerRQ setInfo = new SearchOwnerRQ();
-		setInfo.setCadenaBusqueda(cadenaBusqueda);
-		setInfo.setTipoContacto(tipoContacto);
-		setInfo.setLogPeticionID(log);
-		setInfo.setAlmacenar(almacenar);
-		setInfo.setEjecutivo(ejecutivo);
-		setInfo.setToken(token);
-		setInfo.setLive(live);
-		
-		OwnerInformationSoap servicio = new OwnerInformation().getOwnerInformationSoap();
-		servicio.searchOwner(setInfo);
-		
-		SearchOwnerResponse response = new SearchOwnerResponse();
-		System.out.println(response.getSearchOwnerResult());
 	}
-	
-
 }
